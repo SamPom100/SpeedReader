acbef86fd05b9335477571763afd55df6f31763d
SamPom100 <sampom100@gmail.com>
2020-04-11 15:15:46 -0500

    Update Reader.java
    modified to work on macOS computers

--- Reader.java
+++ Reader.java
@@ -2,39 +2,36 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+
 public class Reader {
     private ArrayList<String> storage = new ArrayList<String>();
-    public ArrayList<String> readText()
-    {
+
+    public ArrayList<String> readText() {
         BufferedReader br = null;
         String contentLine;
         String fullText = "";
-        try
-        { 
-            br = new BufferedReader(new FileReader("C://text.txt"));   
+        try {
+            br = new BufferedReader(new FileReader("text.txt"));
             contentLine = br.readLine();
-            while (contentLine != null)
-            { 
+            while (contentLine != null) {
                 processor(contentLine);
-                contentLine = br.readLine();                 
-            } 
-        } 
-        catch (IOException ioe)
-        {
+                contentLine = br.readLine();
+            }
+        } catch (IOException ioe) {
             ioe.printStackTrace();
-        } 
-        return storage; 
+        }
+        return storage;
     }
 
-    public void processor(String in)
-    {
+    public void processor(String in) {
         String s = in;
-        String[] arr = s.split(" ");       
-        for (String ss : arr)
-        {
+        String[] arr = s.split(" ");
+        for (String ss : arr) {
             storage.add(ss);
         }
     }
-    
-    public ArrayList<String> getStorage(){return storage;}
-} 
+
+    public ArrayList<String> getStorage() {
+        return storage;
+    }
+}
